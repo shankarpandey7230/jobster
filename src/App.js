dfd87f16b3d33f4e2179a36087cdf6fd618331fc
shankarpandey7230 <shankarpandey7230@gmail.com>
2022-11-10 20:26:40 +1100

    setting up routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 import {Landing, Error, Register, Dashboard} from "./pages";
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path ='/' element={<Dashboard/>} />
+        <Route path ='landing' element={<Landing/>} />
+        <Route path ='register' element={<Register/>} />
+        <Route path ='*' element={<Error/>} />
+
+
+      </Routes>
+    </BrowserRouter>
     
   );
 }
