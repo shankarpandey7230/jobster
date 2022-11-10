10ab3f4bbf007fc76e7aae84468b0e1b95c6745c
shankarpandey7230 <shankarpandey7230@gmail.com>
2022-11-10 16:05:53 +1100

    Landing page structure built up

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,7 +3,26 @@ import main from '../assets/images/main.svg';
 
 const Landing = () =>{
     return (
-        <h1>Landing Page</h1>
+        <main>
+            <nav>
+                <img src ={logo} alt = 'jobster logo' className='logo' />
+
+            </nav>
+            <div className='container page'>
+                {/*info*/}
+                <div className='info'>
+                    <h1>job<span>tracking</span>app
+                    </h1>
+                    <p>lorem ipsum dolor sit amet consecteur adispicing elit. Nesciunt place odio nisi,
+                        aperiam ispa natus. Preferendis ispa provident aliquam ex?
+                    </p>
+                    <button className='btn btn-hero'>Login/Register</button>
+
+                </div>
+                <img src ={main} alt='job hunt' className='img main-img'/>
+
+            </div>
+        </main>
     )
 }
 export default Landing;
