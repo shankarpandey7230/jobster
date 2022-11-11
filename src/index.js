940ca62aae11709ab3f82748e06365c01259f235
shankarpandey7230 <shankarpandey7230@gmail.com>
2022-11-12 02:04:50 +1100

    setting up  use slice

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import { createRoot } from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import {store} from './store';
+
+
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App tab='home' />);
+root.render(
+<Provider store ={store}>
+    <App tab='home' />
+</Provider>
+);
