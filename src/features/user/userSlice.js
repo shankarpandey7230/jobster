7a3600b4e7fa8a4a58a1dcf2dbd617501307813b
shankarpandey7230 <shankarpandey7230@gmail.com>
2022-11-12 03:15:35 +1100

    Testing Register

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,5 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import {toast} from 'react-toastify';
+import customFetch from "../../utils/axios";
+
+
 
 const initialState ={
     isLoading :false,
@@ -10,7 +13,15 @@ const initialState ={
 
 export const registerUser = createAsyncThunk('user/registerUser',
  async(user,thunkAPI)=>{
-    console.log(`Register User:${JSON.stringify(user)}`);
+
+    try {
+        const resp = await customFetch.post('/auth/testingRegister',user)
+        console.log(resp)
+    } catch (error) {
+        toast.error(error.response.data.msg)
+        console.log(error.response)
+        
+    }
 
 })
 export const loginUser = createAsyncThunk('user/loginUser',
