4351d0c0a721ee0be6b3acc53f5c8d335b2455c6
shankarpandey7230 <shankarpandey7230@gmail.com>
2022-11-12 00:57:38 +1100

    Handling changes and empty states

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -16,13 +16,21 @@ const initialState={
 function Register(){
     const [values, setValues] = useState(initialState);
     const handleChange =(e)=>{
-        console.log (e.target);
+        const name = e.target.name;
+        const value = e.target.value;
+        
+        setValues({...values, [name]:value});
 
     };
     const onSubmit =(e)=>{
         e.preventDefault();
+        const {name, email, password, isMember} =values
+        if(!email || !password || (!isMember && !name )){
+            console.log ('please  fill out all the feilds');
 
-        console.log(e.target);
+        }
+
+        
 
     };
     const toggleMember =()=>{
