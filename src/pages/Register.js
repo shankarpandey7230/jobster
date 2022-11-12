84f7454d5b95c88034cc10838b00cef652a31843
shankarpandey7230 <shankarpandey7230@gmail.com>
2022-11-13 04:21:49 +1100

    navigating programmatically to dashboard if user is logged in

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -5,6 +5,7 @@ import {FormRow} from '../components';
 import {toast} from 'react-toastify';
 import { useDispatch, useSelector } from 'react-redux';
 import { loginUser, registerUser } from '../features/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 const initialState={
     name:'',
@@ -21,7 +22,7 @@ function Register(){
     const [values, setValues] = useState(initialState);
    const {isLoading, user} =  useSelector (store=> store.user);
    const dispatch = useDispatch();
-
+   const navigate = useNavigate();
     const handleChange =(e)=>{
         const name = e.target.name;
         const value = e.target.value;
@@ -50,6 +51,16 @@ function Register(){
     const toggleMember =()=>{
         setValues({...values, isMember:!values.isMember})
     }
+
+    useEffect (()=>{
+        if(user){
+            setTimeout (()=>{
+                navigate('/');
+
+            },2000)
+        }
+
+    },[user])
 
     return(
         <Wrapper className='full-page'>
