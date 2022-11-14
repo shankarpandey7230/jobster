4a03370595dad5fe237ebd2953b136fa3e67e0bb
shankarpandey7230 <shankarpandey7230@gmail.com>
2022-11-14 18:10:22 +1100

    Css style logic

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,6 +1,12 @@
+import Wrapper from "../assets/wrappers/BigSidebar";
+
+
 const BigSidebar =()=>{
     return (
+
+        <Wrapper>
         <h2> BigSidebar</h2>
+        </Wrapper>
     )
 }
 export default BigSidebar;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
+import Wrapper from "../assets/wrappers/Navbar";
 const Navbar =()=>{
     return(
+        <Wrapper>
         <h2>Navbar</h2>
+        </Wrapper>
     )
 }
 export default Navbar;
