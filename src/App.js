6a3d545987457432b6f3c41a4ca8c427402405ae
shankarpandey7230 <shankarpandey7230@gmail.com>
2022-11-14 21:41:19 +1100

    implications of protected route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {Landing, Error, Register} from "./pages";
+import {Landing, Error, Register,ProtectedRoute } from "./pages";
 import { Profile, AllJobs, AddJob, Stats, SharedLayout } from "./pages/dashboard";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -8,7 +8,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path ='/' element={<SharedLayout/>}>
+        <Route path ='/' element={
+          <ProtectedRoute>
+          <SharedLayout/>
+        </ProtectedRoute>
+        }>
           <Route index element =  {<Stats />}/>
           <Route path = 'all-jobs' element= {<AllJobs />}/>
           <Route path = 'add-job' element= {<AddJob />}/>
