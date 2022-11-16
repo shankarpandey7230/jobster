ac16e8e7e9a46a122a5965e23c913ea8ccb3db91
shankarpandey7230 <shankarpandey7230@gmail.com>
2022-11-16 21:39:55 +1100

    small sidebar setting nav links

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 import Logo from './Logo';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleSidebar } from '../features/user/userSlice';
+import links from '../utils/links';
 
 const SmallSidebar =()=>{
     const {isSidebarOpen} = useSelector((store)=> store.user)
@@ -18,13 +19,31 @@ const SmallSidebar =()=>{
         <Wrapper>
             <div className= {isSidebarOpen ? 'sibar-container show-sidebar': 'sidebar-container'}>
                 <div className='content'>
-                    <button className='close-btn' onClick={toggle}>
+                    <button className='close-btn' onClick={toggle} >
                         <FaTimes />
                     </button>
                     <header>
                         <Logo />
                     </header>
-                    <div className='nav-links'>nav links</div>
+                    <div className='nav-links'>
+                        {links.map ((link)=>{
+                            const {text, path, id, icon} =link
+                            return (
+                            <NavLink to = {path} className={({isActive})=>{
+                                return isActive? 'nav-link active':'nav-link'
+                            }}
+                            key ={id}
+                            onClick ={toggle}
+                            >
+                            <span className='icon'>{icon}  </span>
+                            {text}
+
+                          
+
+                            </NavLink>
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
         </Wrapper>
