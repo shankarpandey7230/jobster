0b662b54b11d755c205bd45c272b3ceb39bc38a7
shankarpandey7230 <shankarpandey7230@gmail.com>
2022-11-24 16:00:13 +1100

    setting up the container for all user jobs

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -1,7 +1,38 @@
+import { useEffect } from 'react';
+import Job from './Job';
+import Wrapper from '../assets/wrappers/JobsContainer';
+import { useSelector, useDispatch } from 'react-redux';
+import Loading from './Loading';
 const JobsContainer =()=>{
+    const {jobs, isLoading } = useSelector((store)=>store.allJobs);
+    const dispatch= useDispatch();
+
+    if(isLoading){
+        return (
+           <Loading  />
+        )
+    }
+    if (jobs.length ===0){
+        return(
+            <Wrapper>
+            <h2> No Jobs to Display</h2>
+
+        </Wrapper>
+        );
+    }
+
     return(
 
-        <h2> Jobs container</h2>
+        <Wrapper>
+            <h5>Jobs info</h5>
+            <div className='jobs'>
+                {jobs.map((job)=>{
+                    console.log(job);
+                    return <Job key ={job.id} {...job} />
+                    
+                })}
+            </div>
+        </Wrapper>
     )
 }
 export default JobsContainer;
