bce89243dd89399bb4b657ba8b84278cbd4a7e46
shankarpandey7230 <shankarpandey7230@gmail.com>
2022-12-03 04:01:06 +1100

    refactoring all jobs

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -1,6 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 import customFetch from '../../utils/axios';
+import { getAllJobsThunk, showStatsThunk } from './allJobThunk';
 
 const initialFiltersState = {
   search: '',
@@ -22,38 +23,11 @@ const initialState = {
 };
 
 export const getAllJobs = createAsyncThunk(
-  'allJobs/getJobs',
-  async (_, thunkAPI) => {
-
-    const {page, search, searchStatus, searchType,sort}= thunkAPI.getState().allJobs;
-    let url = `/jobs?status=${searchStatus}&jobType=${searchType}&sort=${sort}&page=${page}`;
-    if(search){
-      url = url+`&search= ${search}`;
-    }
-
-    try {
-      const resp = await customFetch.get(url);
-    
-      
-      return resp.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.data.msg);
-    }
-  }
-);
+  'allJobs/getJobs',getAllJobsThunk);
 
 
 export const showStats = createAsyncThunk(
-  'allJobs/showStats',
-  async (_, thunkAPI) => {
-    try {
-      const resp = await customFetch.get('/jobs/stats');
-      
-      return resp.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.data.msg);
-    }}
-  );
+  'allJobs/showStats',showStatsThunk);
 
 
 const allJobsSlice = createSlice({
