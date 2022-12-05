47346c948014cb3039a68fbb4a0e5c28d873043b
shankarpandey7230 <shankarpandey7230@gmail.com>
2022-12-05 16:51:01 +1100

    setting up debounce

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -3,8 +3,10 @@ import { FormRow, FormRowSelect } from '.';
 import Wrapper from '../assets/wrappers/SearchContainer';
 import { useSelector, useDispatch } from 'react-redux';
 import { handleChange,clearFilters } from '../features/allJobs/allJobsSlice';
+import { useMemo, useState } from 'react';
 
 const SearchContainer =()=>{
+  const [localSearch, setLocalSearch] = useState('');
     const {isLoading, search, searchStatus, searchType, sort,sortOptions}=
     useSelector((store)=>store.allJobs);
 
@@ -22,6 +24,18 @@ const SearchContainer =()=>{
         e.preventDefault();
         dispatch(clearFilters());
       };
+      const debounce = () => {
+        let timeoutID;
+        return (e) => {
+          setLocalSearch(e.target.value);
+          clearTimeout(timeoutID);
+          timeoutID = setTimeout(() => {
+            dispatch(handleChange({ name: e.target.name, value: e.target.value }));
+          }, 1000);
+        };
+      };
+      const optimizedDebounce = useMemo (()=> debounce(),[])
+
     return(
         <Wrapper>
             <form className='form'>
@@ -32,8 +46,8 @@ const SearchContainer =()=>{
                     <FormRow 
                     type ='text' 
                     name='search' 
-                    value={search} 
-                    handleChange = {handleSearch}
+                    value={localSearch} 
+                    handleChange = {optimizedDebounce}
                     />
                     <FormRowSelect
             labelText='status'
